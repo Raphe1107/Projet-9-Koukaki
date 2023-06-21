2824fb8136ceb73c17abd746aa7497780acddd09
Raphe1107 <rnuguet@gmail.com>
2023-06-21 17:07:44 +0200

    fade-in

--- wp-content/themes/foce-child/nominations.php
+++ wp-content/themes/foce-child/nominations.php
@@ -3,6 +3,6 @@
         <h2>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h2>
     </div>
     <div class="nomination-img">
-        <img src="<?php echo get_stylesheet_directory_uri() .'/img/nominations.png'?>" alt="nomination">
+        <img src="<?php echo get_stylesheet_directory_uri() .'/assets/nominations.png'?>" alt="nomination">
     </div> 
 </section>
